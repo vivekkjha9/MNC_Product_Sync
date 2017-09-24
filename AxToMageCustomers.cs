c6a4567fbd1b58a2cef2c212c48fe17553356eee
Admine8e276ddfc <Admine8e276ddfc@REP-DEV-1>
2017-09-24 13:06:08 +0000

    final version - AX-Magento Customer Integtaion- Pull Scenario

--- AxToMageCustomers.cs
+++ AxToMageCustomers.cs
@@ -42,9 +42,9 @@ namespace AX_CRT_Mage_Connector
             };
 
             
-            writer.Write("Dynamics Connectivity validation starting...");
+            
             var crtManager = new DynamicsRuntimeManager();
-            //var  nv_CustomerLists = client.ReadMultiple(null, null, 10000);
+           
 
             Microsoft.Dynamics.Commerce.Runtime.PagedResult<Microsoft.Dynamics.Commerce.Runtime.DataModel.Customer> axcustomers = ReadAxCustomers.readCustomer(crtManager,writer);
             AX_CRT_MAge_Connector.MagentoConnectService.PortTypeClient mage_client = new AX_CRT_MAge_Connector.MagentoConnectService.PortTypeClient();
@@ -63,8 +63,29 @@ namespace AX_CRT_Mage_Connector
                 try
                 {
 
-                    customerCreate.firstname = axcustomers.Results[i].FirstName;
-                    customerCreate.lastname = axcustomers.Results[i].LastName;
+                   
+                    if (axcustomers.Results[i].FirstName=="")
+                    { 
+                    db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "ERROR: First Name not maintained in AX.This customer will not be integrated", "FAILED");
+                    continue;
+                    }
+                    else
+                    {
+                        customerCreate.firstname = axcustomers.Results[i].FirstName;
+                    }
+
+                    if (axcustomers.Results[i].LastName == "")
+                    {
+                        db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "ERROR: Last Name not maintained in AX.This customer will not be integrated", "FAILED");
+                        continue;
+                    }
+                    else
+                    {
+                        customerCreate.firstname = axcustomers.Results[i].LastName;
+                    }
+
+
+                   // customerCreate.lastname = axcustomers.Results[i].LastName;
                     customerCreate.middlename = "";
                     customerCreate.password = "abc123";
                     customerCreate.store_id = 1;
@@ -74,13 +95,24 @@ namespace AX_CRT_Mage_Connector
                     customerCreate.group_id = 1;
                     customerCreate.group_idSpecified = true;
                     customerCreate.suffix = "P";
-                    customerCreate.email = axcustomers.Results[i].Email;
+                    if( axcustomers.Results[i].Email.Equals(""))
+                    {
+
+                        db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "ERROR: Email Id not maintained in AX.This customer will not be integrated", "FAILED");
+                        continue;
+                    }
+                    else
+                    {
+                        customerCreate.email = axcustomers.Results[i].Email;
+                    }
                     customerCreate.gender = 1; //1-Male;2-Female
                     customerCreate.genderSpecified = true;
+                    
                     //customer address
                     customerAddressEntityCreate customerAddress = new customerAddressEntityCreate();
                     if (axcustomers.Results[i].Addresses[0].City == null)
                     {
+                        db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "WARNING: City not maintained in AX.For Magento System it is a mandatory field and hence system will default it to NA. You can change it later", "WARNING");
                         customerAddress.city = "NA";
                     }
                     else
@@ -89,8 +121,9 @@ namespace AX_CRT_Mage_Connector
                     }
                     customerAddress.region_id = 2;   //CountryID
                     customerAddress.region_idSpecified = true;
-                    if (axcustomers.Results[i].Addresses[0].TwoLetterISORegionName == null)
+                    if (axcustomers.Results[i].Addresses[0].TwoLetterISORegionName == "")
                     {
+                        db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "WARNING: Country information is not maintained in AX.For Magento System it is a mandatory field and hence system will default it to NA. You can change it later", "WARNING");
                         customerAddress.country_id = "NA";
                     }
                     else
@@ -101,24 +134,39 @@ namespace AX_CRT_Mage_Connector
                     customerAddress.firstname = axcustomers.Results[i].FirstName;
                     customerAddress.lastname = axcustomers.Results[i].LastName;
                     customerAddress.middlename = axcustomers.Results[i].MiddleName;
-                    string[] streettxt = { axcustomers.Results[i].Addresses[0].StreetNumber };
+                    string[] streettxt = { axcustomers.Results[i].Addresses[0].FullAddress };
                     customerAddress.street = streettxt;
-
-                    customerAddress.postcode = axcustomers.Results[i].Addresses[0].ZipCode;
+                    if (axcustomers.Results[i].Addresses[0].ZipCode=="")
+                    {
+                        db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "WARNING: Postal Code not maintained in AX.For Magento System it is a mandatory field and hence system will default it to 99999. You can change it later", "WARNING");
+                        customerAddress.postcode = "99999";
+                    }
+                    else
+                    {
+                        customerAddress.postcode = axcustomers.Results[i].Addresses[0].ZipCode;
+                    }                    
                     customerAddress.prefix = "M";
                     customerAddress.suffix = "P";
                     //  customerAddress.street = nv_CustomerLists[i].Address.ToString();
                     if (axcustomers.Results[i].Addresses[0].Phone == null)
                     {
-                        customerAddress.telephone = "NA";
+                       // customerAddress.telephone = "NA";
                     }
                     else
                     {
+                        if (axcustomers.Results[i].Addresses[0].Phone.Equals(""))
+                        {
+                            db.InsertLog("Customer", "AX Account Number: " + axcustomers.Results[i].AccountNumber, "WARNING: Phone not maintained in AX.For Magento System it is a mandatory field and hence system will default it to 999-999-999. You can change it later", "WARNING");
+                            customerAddress.telephone = "999-999-999";
+                        }
+                        else { 
                         customerAddress.telephone = axcustomers.Results[i].Addresses[0].Phone;
+                        }
                     }
                     //If you specify 'true' for is_default_billing and is_default_shipping. Then this address will be your default shipping and billing. Otherwise none. 
                     customerAddress.is_default_billing = true;
                     customerAddress.is_default_shipping = true;
+                   // customerAddress.telephone = axcustomers.Results[i].Addresses[0].Phone;
 
                     if (db.fetch_Customer(axcustomers.Results[i].AccountNumber.ToString()))
                     {
@@ -131,14 +179,17 @@ namespace AX_CRT_Mage_Connector
 
                     }
                     db.InsertCustomerMapping(axcustomers.Results[i].AccountNumber, newCustomerCreateID.ToString(), axcustomers.Results[i].Email);
-                    db.InsertLog("Customer", axcustomers.Results[i].AccountNumber, "Navision Customer Code", "SUCCESS");
+                    db.InsertLog("Customer", axcustomers.Results[i].AccountNumber, "AX Customer Account Number", "SUCCESS");
                 }
                 catch (Exception ex)
                 {
                     AX_CRT_MAge_Connector.ErrorLog errLog = new AX_CRT_MAge_Connector.ErrorLog();
-
-                    errLog.LogError("C:\\MNC_Logs", "Customer :" + ex.Message);
-                   // db.InsertLog("Customer", nv_CustomerLists[i].No, ex.Message, "FAILED");
+                    if (newCustomerCreateID!=0)
+                    {
+                        mage_client.customerCustomerDelete(token_id, newCustomerCreateID);
+                    }
+                    //errLog.LogError("C:\\MNC_Logs", "Customer :" + ex.Message);
+                    db.InsertLog("Customer", axcustomers.Results[i].AccountNumber, ex.Message, "FAILED");
 
 
                 }
